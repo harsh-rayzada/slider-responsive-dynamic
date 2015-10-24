b1e452ce793322bd9fe4b079f43a5957ce4c6cb4
nitish279 <nitish@mantralabsglobal.com>
2015-10-24 13:35:09 +0530

    nitish chages

--- resp-slide/assets/js/main.js
+++ resp-slide/assets/js/main.js
@@ -15,7 +15,39 @@ $(document).ready(function() {
 });
 
 $(document).bind('mobileinit',function(){
-          $.mobile.changePage.defaults.changeHash = false;
-          $.mobile.hashListeningEnabled = false;
-          $.mobile.pushStateEnabled = false;
-      });
+    $.mobile.changePage.defaults.changeHash = false;
+    $.mobile.hashListeningEnabled = false;
+    $.mobile.pushStateEnabled = false;
+});
+
+$( document ).ready(function(){
+  $('#myCarousel').carousel({
+      interval: 4000
+    })
+
+    $('.carousel .item').each(function(){
+      var next = $(this).next();
+      if (!next.length) {
+        next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+      if ( $(window).width() < 768){
+        for (var i=0;i<0;i++) {
+          next=next.next();
+          if (!next.length) {
+            next = $(this).siblings(':first');
+          }
+          next.children(':first-child').clone().appendTo($(this));
+        }
+      }
+      if( $(window).width() > 768 ){
+          for (var i=0;i<1;i++) {
+          next=next.next();
+          if (!next.length) {
+            next = $(this).siblings(':first');
+          }
+          next.children(':first-child').clone().appendTo($(this));
+        }
+      }
+    });
+});
